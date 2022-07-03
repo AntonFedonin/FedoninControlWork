72c293f958cede8b381993dc48cafb7891c3c84d
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2022-07-03 23:40:46 +0300

    Create data block

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+//  длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//  лучше обойтись исключительно массивами.
+
+Console.Write("Задайте размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+string[] array = new string[arraySize];
+
+void FillArray(string[]collection)
+{
+    
+}
